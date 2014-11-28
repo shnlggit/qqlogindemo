59b01fc6c905d4a8749ed92e910e55359e23cd44
shenliang <shenliang@tose.com.cn>
2014-11-28 15:19:29 +0800

    qq sdk

---
+++ demo/index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+    <title>test</title>
+  </head>
+  <body>
+hello world!
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+<h2>欢迎使用QQ Connet SDK For PHP 2.1</h2>
+请先<a href="install/">设置配置项</a><br />
+或者打开doc目录下，阅读我们的文档
+<br>
+<a href="demo/">test demo</a>
+</body>
+</html>
